e6e59182a7dffe906fff3ba6b8bf23507c1c9582
Zenghu <978689162@qq.com>
2019-11-03 19:30:04 +0800

    create project

---
+++ src/components/Login.vue
@@ -0,0 +1,82 @@
+<template>
+
+    <form id="login" v-on:submit.prevent="onSubmit">
+        <label id="title">登录</label>
+        <input type="text" id="name" placeholder="username" v-model="username"/>
+        <br/>
+        <input type="password" id="pwd" placeholder="password" v-model="password"/>
+        <br/>
+
+        <input type="text" placeholder="验证码" v-model="checkCode">
+        <input type="submit" value="login"/>
+        <label id="error">{{error}}</label>
+    </form>
+</template>
+
+<script>
+export const evidenceUrl = process.env.API_ROOT
+export default {
+  name: 'login',
+  data () {
+    return {
+      username: '',
+      password: '',
+      error: '',
+      checkCode: ''
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('username:' + this.username)
+      var url = evidenceUrl + '/account/rest/login'
+      console.log('url:' + url)
+      var formData = new FormData()
+      formData.append('username', this.username)
+      formData.append('password', this.password)
+      let config = {
+        emulateJSON: true
+      }
+      this.$http.post(url, formData, config).then(function (response) {
+        if (response.body.status === '400') {
+          this.error = response.body.msg
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style>
+    #login {
+        position: absolute;
+        margin-left: 40%;
+        width: 240px;
+        height: 240px;
+        margin-top: 15%;
+        background-color: deepskyblue;
+    }
+
+    #title {
+        position: relative;
+        margin-top: 30px;
+        margin-left: 40%;
+        font: 20px bold;
+        text-align: center;
+    }
+
+    input {
+        position: relative;
+        width: 200px;
+        margin-top: 20px;
+        margin-left: 8%;
+        height: 30px;
+        font-size: 15px;
+        font-weight: bold;
+    }
+
+    #error {
+        position: absolute;
+        color: red;
+        display: flex;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import router from './router'
+import VueResource from 'vue-resource'
+import Login from './components/Login'
+
+Vue.use(VueResource)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { Login },
+  template: '<Login/>'
+})
