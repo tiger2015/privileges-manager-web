393089d3f2a067c99a1cda83e910a2e7f4e32b27
zeng.h <zeng.h@starcart.cn>
2019-11-07 18:57:29 +0800

    add permission add

---
+++ src/components/permission/AddPermission.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <form>
+          <table>
+            <tr>
+              <td>名称</td>
+              <td><input type="text"  v-model="permission.name" required/></td>
+            </tr>
+             <tr>
+              <td>url</td>
+              <td><input  type="text"  v-model="permission.url" required/></td>
+            </tr>
+             <tr>
+              <td>desc</td>
+              <td><textarea v-model="permission.desc"></textarea></td>
+            </tr>
+             <tr>
+              <td colspan="2"><input type="submit" value="提交"></td>
+            </tr>
+          </table>
+       </form>
+    </div>
+</template>
+<script>
+export default {
+  name: 'addPermission',
+  data () {
+    return {
+      permission: {name: '', url: '', desc: ''}
+    }
+  }
+}
+</script>
---
+++ src/components/permission/UpdatePermission.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+        <form>
+           <label>名称</label><input type="text"  v-model="persmission.name"/><br/>
+           <label>ulr</label><input  type="text"  v-model="persmission.ulr"/><br/>
+           <label>desc</label><textarea v-model="permission.desc"></textarea><br/>
+           <input type="submit" value="提交">
+       </form>
+    </div>
+</template>
+<script>
+export default {
+  name: 'updatePermission',
+  data () {
+    return {
+      permission: {name: '', url: '', desc: ''}
+    }
+   }
+}
+</script>
